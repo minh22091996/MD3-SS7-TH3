94c3efe2eed58122b53fa14dc56e56dccbfae366
minh22091996 <tranbinhminh22091996@gmail.com>
2023-03-10 00:08:07 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(3.6);
+        circles[1] = new Circle();
+        circles[2] = new Circle(3.5, "indigo", false);
+
+
+        System.out.println("Pre-sorted:");
+        for (Circle circle1 : circles) {
+            System.out.println(circle1);
+        }
+
+
+        Comparator circleComparator = new CircleComparator();
+        Arrays.sort(circles, circleComparator);
+
+
+        System.out.println("After-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle1);
+        }
+
+    }
+}
